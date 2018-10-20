0c2db76af51341a361e1639bba00b67c4bd7b359
adrianyim <adriany93@gmail.com>
2018-10-20 12:42:42 -0600

    Done with Prove 04

--- Prove04.py
+++ Prove04.py
@@ -1,49 +1,98 @@
 import pandas as pd
 import numpy as np
+import graphviz
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from random import randint as rand
 from sklearn import tree
-import graphviz
+from sklearn.metrics import accuracy_score
+import random
+from sklearn.preprocessing import LabelEncoder
 
-# Iris Decision Tree function
+#-- Iris Decision Tree function --
 def getIrisTree():
     iris = load_iris()
 
     # Splitting the dataset into the Training set and Test set
-    data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target, train_size = 0.7, test_size = 0.3)
+    data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target, test_size = 0.3)
 
-    skTree = tree.DecisionTreeClassifier()
+    # Calling the decision tree classifier from the sklearn
+    skTree = tree.DecisionTreeClassifier(max_depth=4)
     skTree = skTree.fit(data_train, target_train)
 
-    dot_data = tree.export_graphviz(skTree, out_file = None, feature_names=iris.feature_names, class_names=iris.target_names, filled=True, rounded=True, special_characters=True)
+    # Exporting a tree by using graphviz
+    dot_data = tree.export_graphviz(skTree, out_file=None, feature_names=iris.feature_names, class_names=iris.target_names, filled=True, rounded=True, special_characters=True)
     graph = graphviz.Source(dot_data)
     graph.render("iris")
-    print(graph)
 
-# Lenses Decision Tree function
+    # Predicting and testing the accuracy, and then print it out
+    targets_predicted = skTree.predict(data_test)
+    print("The iris accuracy is", round(accuracy_score(target_test, targets_predicted)*100, 1), "%")
+
+#-- Lenses Decision Tree function --
 def getLensesTree():
-    lenses = np.array(pd.read_csv("lenses.csv", header=None))
-    data = lenses[:,0:-1]
-    target = lenses[:,-1]
-    print(lenses)
+    lenses = pd.read_fwf("lenses.txt", sep=" ", header=None)
+    lenses.columns = ["patient-num", "age", "perscription", "astilmatic", "tear-rate", "classif"]
 
+    # Assigning the dataset into data and target
+    target = lenses.iloc[:,5].values
+    data = lenses.iloc[:,1:].values
 
-def handleNonNum(data):
-    stringMap = {}
-    it = 0
-    for (x, y), value in np.ndenumerate(data):
-        if not value.isdigit():
-            if not value in stringMap:
-                stringMap[data[x][y]] = it
-                it += 1
-            data[x][y] = stringMap[value]
+    # Splitting the dataset into the Training set and Test set
+    data_train, data_test, target_train, target_test = train_test_split(data, target, test_size = 0.3)
 
-def handleMissingData(data):
-    for (x, y), value in np.ndenumerate(data):
-       if value == "?" or value == "NA":
-           while data[x][y] == "?" or data[x][y] == "NA":
-               data[x][y] = data[rand(0, len(data) - 1)][y]
+    # Calling the decision tree classifier from the sklearn
+    skTree = tree.DecisionTreeClassifier(max_depth=4)
+    skTree = skTree.fit(data_train, target_train)
 
-#getIrisTree()
+    # Exporting a tree by using graphviz
+    dot_data = tree.export_graphviz(skTree, out_file=None, filled=True, rounded=True, special_characters=True)
+    graph = graphviz.Source(dot_data)
+    graph.render("lenses")
+    
+    # Predicting and testing the accuracy, and then print it out
+    targets_predicted = skTree.predict(data_test)
+    print("The lenses accuracy is", round(accuracy_score(target_test, targets_predicted)*100, 1), "%")
+
+#-- Vote Decision Tree function --
+def getVoteTree():
+    vote = pd.read_csv("vote.csv", sep=",", header=None)
+    #vote.columns = ["Class Name", "Handicapped-infants", "Water-project-cost-sharing", "Adoption-of-the-budget-resolution", "Physician-fee-freeze", "El-salvador-aid", "Religious-groups-in-schools", "Anti-satellite-test-ban", "Aid-to-nicaraguan-contras", "Mx-missile", "immigration", "Synfuels-corporation-cutback", "Education-spending", "Superfund-right-to-sue", "Crime", "Duty-free-exports", "Export-administration-act-south-africa"]
+
+    # Assiging a missing data in "y" and "n" randomly
+    vote = vote.replace("?", np.random.choice(["y","n"]))
+
+    # Assigning the dataset into data and target
+    data = vote.iloc[:,1:].values
+    target = vote.iloc[:,0].values
+
+    # Convert data into a 1-D array
+    target = target.ravel()
+
+    # Normalizing class data with a labelEncoder()
+    LEncoder = LabelEncoder()
+    target = LEncoder.fit_transform(target)
+
+    # Normalizing attributes using same process
+    for i in range(data.shape[1]):
+        data[:,i] = LEncoder.fit_transform(data[:,i])
+
+    # Splitting the dataset into the Training set and Test set
+    data_train, data_test, target_train, target_test = train_test_split(data, target, test_size = 0.3)
+
+    # Calling the decision tree classifier from the sklearn
+    skTree = tree.DecisionTreeClassifier(max_depth=4)
+    skTree = skTree.fit(data_train, target_train)
+
+    # Exporting a tree by using graphviz
+    dot_data = tree.export_graphviz(skTree, out_file=None, filled=True, rounded=True, special_characters=True)
+    graph = graphviz.Source(dot_data)
+    graph.render("vote")
+
+    # Predicting and testing the accuracy, and then print it out
+    targets_predicted = skTree.predict(data_test)
+    print("The voteing accuracy is", round(accuracy_score(target_test, targets_predicted)*100, 1), "%")
+               
+getIrisTree()
 getLensesTree()
+getVoteTree()
